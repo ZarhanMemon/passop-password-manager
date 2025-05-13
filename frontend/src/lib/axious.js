8e88f17293fc4fd36821e3917d4637db99ad8bd7
Zarhan Memon <ironmanred118@gmail.com>
2025-05-13 17:24:21 +0530

    Update axious.js

--- frontend/src/lib/axious.js
+++ frontend/src/lib/axious.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 
-export const axiosInstance = axios.create({
+export const instances = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // Required for cookies to be sent
 });
